4baa2fbb7827bf9951909850610e536faa1907c5
Assen95 <asenjo95@gmail.com>
2023-05-01 14:09:05 +0300

    third commit

--- Petstagram/pets/views.py
+++ Petstagram/pets/views.py
@@ -5,13 +5,13 @@ def add_pet(request):
     return render(request, template_name='pets/pet-add-page.html')
 
 
-def details_pet(request, pk, pet_name):
+def details_pet(request, username, pet_name):
     return render(request, template_name='pets/pet-details-page.html')
 
 
-def edit_pet(request, pk, pet_name):
+def edit_pet(request, username, pet_name):
     return render(request, template_name='pets/pet-edit-page.html')
 
 
-def delete_pet(request, pk, pet_name):
+def delete_pet(request, username, pet_name):
     return render(request, template_name='pets/pet-delete-page.html')
